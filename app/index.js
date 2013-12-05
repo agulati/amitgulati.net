c22f8a2e8d1239323172f738156c92e9f163d635
Amit Gulati <amit@moven.com>
2013-12-05 15:01:24 -0500

    Initial commit
    New node.js server using express and Handlebars templates

---
+++ app/index.js
@@ -0,0 +1,25 @@
+var http = require('http')
+  , fs = require('fs')
+  , async = require('async')
+  , express = require('express')
+  , _ = require('underscore')
+
+require('express-namespace');
+
+var app = express()
+
+app.configure(function () {
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'hbs')
+  app.set('port', 5000)
+  app.use(app.router)
+})
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+var httpServer = http.createServer(app).listen(app.get('port'), function () {
+  console.log("HTTP server listening on port %s in %s mode", app.get('port'), app.settings.env)
+})
+
